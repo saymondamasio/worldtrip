6a8b9ca39e3e1a5dd4867929ae086c2682ad89e9
Saymon Damásio <saymon.damasio95@gmail.com>
2021-12-26 12:13:21 -0300

    feat: add fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,14 @@ export default class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head></Head>
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap"
+            rel="stylesheet"
+          ></link>
+        </Head>
 
         <body>
           <Main />
